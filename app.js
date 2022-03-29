5deea6242490e22cdb215844e8fb306629788efb
NikhilGithubOfficial <nkmnikhil0009@gmail.com>
2022-03-29 12:39:44 +0530

    added comments

--- app.js
+++ app.js
@@ -1,59 +1,64 @@
 const schedule = require('node-schedule');
 
-const scheduleList = [
+// list of events
+const events = [
 	{
-		"text": "textOne",
-		"dateTime": "2022-03-28T23:38:00.000+05:30"
+		"text": "TextOne",
+		"dateTime": "2022-03-31T23:38:00.000+05:30"
     },
     {
-		"text": "textTwo",
-		"dateTime": "2022-03-28T23:39:00.000+05:30"
+		"text": "TextTwo",
+		"dateTime": "2022-03-31T23:39:00.000+05:30"
     },
     {
-		"text": "textThree",
-		"dateTime": "2022-03-28T23:40:00.000+05:30"
+		"text": "TextThree",
+		"dateTime": "2022-03-31T23:40:00.000+05:30"
     },
     {
-		"text": "textFour",
-		"dateTime": "2022-03-28T23:41:00.000+05:30"
+		"text": "TextFour",
+		"dateTime": "2022-03-31T23:41:00.000+05:30"
     },
     {
-		"text": "textFive",
-		"dateTime": "2022-03-28T23:42:00.000+05:30"
+		"text": "TextFive",
+		"dateTime": "2022-03-31T23:42:00.000+05:30"
     },
     {
-		"text": "textSix",
-		"dateTime": "2022-03-28T23:43:00.000+05:30"
+		"text": "TextSix",
+		"dateTime": "2022-03-31T23:43:00.000+05:30"
     },
     {
-		"text": "textSeven",
-		"dateTime": "2022-03-28T23:44:00.000+05:30"
+		"text": "TextSeven",
+		"dateTime": "2022-03-31T23:44:00.000+05:30"
     },
     {
-		"text": "textEight",
-		"dateTime": "2022-03-28T23:45:00.000+05:30"
+		"text": "TextEight",
+		"dateTime": "2022-03-31T23:45:00.000+05:30"
     },
     {
-		"text": "textNine",
-		"dateTime": "2022-03-28T23:46:00.000+05:30"
+		"text": "TextNine",
+		"dateTime": "2022-03-31T23:46:00.000+05:30"
     },
     {
-		"text": "textTen",
-		"dateTime": "2022-03-28T23:47:00.000+05:30"
+		"text": "TextTen",
+		"dateTime": "2022-03-31T23:47:00.000+05:30"
     }
 ]
 
 function trigger(text, dateTime) {
-    let size = text.length;
-    let reverseText = text.split("").reverse().join("");
+    let size = text.length; // store the length of text in size
+    let reverseText = text.split("").reverse().join(""); // reverse the text string
     let scheduledTime = new Date(dateTime);
+    // scheduling the event to trigger the function at the said date and time
     schedule.scheduleJob(scheduledTime, () => {
+        // function will sleep for size (the length of text) seconds
+        // and then output reverseText in the console
         setTimeout(function() {
             console.log(reverseText);
         }, 1000 * size);
     })
 }
 
-scheduleList.forEach((item,index) => {
+// looping through the list of events
+events.forEach((item,index) => {
     trigger(item.text,item.dateTime);
 });
